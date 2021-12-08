b9cded7c6543d9dd11b253f98bbd63c62391164f
Suraj Bobade <iamsurajbobade@gmail.com>
2021-12-08 17:00:46 +0530

    End of `E2`

--- handlers/goodbye.go
+++ handlers/goodbye.go
@@ -1,8 +1,6 @@
 package handlers
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -11,16 +9,10 @@ type Goodbye struct {
 	log *log.Logger
 }
 
-func NewGoodbye(log *log.Logger) *Hello {
-	return &Hello{log: log}
+func NewGoodbye(log *log.Logger) *Goodbye {
+	return &Goodbye{log: log}
 }
 
 func (Goodbye *Goodbye) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	Goodbye.log.Println("Good bye!")
-	data, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		http.Error(rw, "Oops", http.StatusBadRequest)
-		return
-	}
-	fmt.Fprintf(rw, "Hello %s", data)
+	rw.Write([]byte("Byee"))
 }
--- main.go
+++ main.go
@@ -1,21 +1,46 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/IAmSurajBobade/go_microservices/handlers"
 )
 
 func main() {
-	logger := log.New(os.Stdout, "go-micro-api", log.LstdFlags)
+	logger := log.New(os.Stdout, "go-micro-api - ", log.LstdFlags)
 	hw := handlers.NewHello(logger)
 	gb := handlers.NewGoodbye(logger)
 
 	mux := http.NewServeMux()
-	mux.Handle("/", hw)
 	mux.Handle("/goodbye", gb)
+	mux.Handle("/hello", hw)
 
-	http.ListenAndServe(":9090", mux)
+	server := &http.Server{
+		Addr:         ":9090",
+		Handler:      mux,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			logger.Fatal(err)
+		}
+	}()
+
+	signalChannel := make(chan os.Signal)
+	signal.Notify(signalChannel, os.Interrupt)
+	signal.Notify(signalChannel, os.Kill)
+
+	slg := <-signalChannel
+	logger.Println("Received terminate, graceful shutdown", slg)
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	server.Shutdown(tc)
 }
