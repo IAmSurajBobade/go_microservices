738b6516fa63c8d5775ec336ca1a294f410f8fe6
Suraj Bobade <iamsurajbobade@gmail.com>
2021-12-08 14:29:17 +0530

    `E2` part

---
+++ handlers/helloworld.go
@@ -0,0 +1,26 @@
+package handlers
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Hello struct {
+	log *log.Logger
+}
+
+func NewHello(log *log.Logger) *Hello {
+	return &Hello{log: log}
+}
+
+func (hello *Hello) ServeHTTP(rw http.ResponseWriter, r http.Request) {
+	hello.log.Println("Hello world!")
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(rw, "Ooops", http.StatusBadRequest)
+		return
+	}
+	fmt.Fprintf(rw, "Hello %s", data)
+}
--- main.go
+++ main.go
@@ -1,24 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/IAmSurajBobade/go_microservices/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Hello world!")
-		data, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(rw, "Ooops", http.StatusBadRequest)
-			return
-		}
-		fmt.Fprintf(rw, "Hello %s", data)
-	})
-	http.HandleFunc("/goodbye", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Goodbye world!")
-	})
+	logger := log.New(os.Stdout, "awesome-go", log.Flags())
+	hw := handlers.NewHello(logger)
+
 	http.ListenAndServe(":9090", nil)
 }
